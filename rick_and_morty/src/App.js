3c6bf79c4c86f5660ce924c2f235c8d1d037437f
titooswaldo <titooswaldobriceno@gmail.com>
2023-06-14 21:32:05 -0500

    empezamos practica 8 cliclo de vida del componente

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -13,16 +13,6 @@ function App() {
       <NavBar/>
       <Filters/>
       <Cards characters={characters} />
-      {/* <Card
-        id={Rick.id}
-        name={Rick.name}
-        status={Rick.status}
-        species={Rick.species}
-        gender={Rick.gender}
-        origin={Rick.origin.name}
-        image={Rick.image}
-        onClose={() => window.alert('Emulamos que se cierra la card')}
-      /> */}
     </div>
   );
 }
